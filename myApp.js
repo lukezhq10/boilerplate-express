0ede20dda529c2f8e79ed5cfb95574ceab971da2
Luke Zhang <you@example.com>
2023-03-20 22:09:59 -0600

    add middleware logger

--- myApp.js
+++ myApp.js
@@ -1,8 +1,26 @@
+require('dotenv').config()
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 let express = require('express');
 let app = express();
-console.log("Hello World")
+console.log("Hello World");
+
+var middleware_logger = (req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+};
+app.use(middleware_logger);
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 app.use("/public", express.static(__dirname + '/public'));
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        res.json({"message": "HELLO JSON"});
+    }   
+    else {
+        res.json({"message": "Hello json"});
+    }
+});
 module.exports = app;
