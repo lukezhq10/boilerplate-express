e00c403a3eb02657069347dda3e5bdf4d5fe1259
Luke Zhang <you@example.com>
2023-03-20 21:13:12 -0600

    add static asset

--- myApp.js
+++ myApp.js
@@ -1,39 +1,8 @@
 let express = require('express');
 let app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("Hello World")
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+app.use((__dirname + '/public'), express.static());
  module.exports = app;
